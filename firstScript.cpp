23442d1ca87659f8e77561e5e0fa9b029dcad4e1
nower16 <nuzhatnower@iut-dhaka.edu>
2023-12-20 18:27:15 +0600

    first commit

---
+++ firstScript.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num = 0;
+    cout << num << endl;
+    cout << "HELLO WORLD" << endl;
+    cin >> num;
+    cout << num << endl;
+}
