0f3c4e3c26514662ed860495ac494dad12aafa31
nower16 <nuzhatnower@iut-dhaka.edu>
2023-12-25 22:02:22 +0600

    arithmetic

---
+++ arithmetic.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main (){
+    int num1, num2;
+    cout<<"Enter two numbers :"<<endl;
+    cin>> num1>> num2 ;
+
+
+    int sum = num1 + num2;
+    cout<< "sum is :"<< sum<<endl;
+
+    int sub = num1 - num2;
+    cout<< "sub is :"<< sub<<endl;
+
+    int mul = num1 * num2;
+    cout<< "mul is :"<< mul<<endl;
+
+
+    double div = (float)num1 / num2;
+    cout<< "div is :"<< div<<endl;
+
+    int rem = num1 % num2;
+    cout<< "rem is :"<<rem<<endl;
+
+    getch();
+  
+}
