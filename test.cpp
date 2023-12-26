3953b3f0af86a5124f2f640fc868c16e5d1e479c
nower16 <nuzhatnower@iut-dhaka.edu>
2023-12-27 01:40:06 +0600

    unary and bitwise

--- test.cpp
+++ test.cpp
@@ -13,11 +13,11 @@ int main(){
     cin>>num;
 
     // loop that many numbers of times
-    // for ( int i= 0; i<num ;i=i+1 ){
-    //     // output if that loop fulfills or it doesn't
-    //     cout<<love<<endl;
+    for ( int i= 0; i<num ;i=i+1 ){
+        // output if that loop fulfills or it doesn't
+        cout<<love<<endl;
         
-    // } 
+    } 
 
     
 
---
+++ unary.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+
+    int x=3;
+
+    int y = x--; //x=3
+
+    cout<<x<<endl; //pore jokhn abar x pabe tokhn decrement hobe x=2
+    cout<<y;
+
+    getch();
+}
