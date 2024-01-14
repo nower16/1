7cb9691cec26ccd61e6dd1b773329c583de9bcf3
nower16 <nuzhatnower@iut-dhaka.edu>
2024-01-15 01:21:17 +0600

    PLYMOR

--- stringLibrary.cpp
+++ stringLibrary.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <conio.h>
 #include <cstring>
+#include <string>
 using namespace std;
 
 int main()
@@ -9,19 +10,29 @@ int main()
     char name2[] = " Nower";
     char name3[] = " nower";
 
-    int len = strlen(name1);
-    cout << "Length of string: " << len << endl;
-
     strcat(name1, name2);
     cout << name1 << endl;
 
     int value = strcmp(name1, name2);
 
     if (value == 0)
-        cout << "Srings are equal";
+        cout << "Srings are equal" << endl;
 
     else
-        cout << "Srings are not equal";
+        cout << "Srings are not equal" << endl;
+
+    string str1 = "tamzid";
+    string str2 = " ahmed";
+    string str3;
+
+    str3 = str1;
+    cout << "str3 = " << str3 << endl;
+
+    str3 = str1 + str2;
+    cout << "str1+str2 =" << str3 << endl;
+
+    int len = str1.size();
+    cout << "Length of str1 = " << len << endl;
 
     getch();
 }
